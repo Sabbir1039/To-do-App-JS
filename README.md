7d4599654be2d620680c602bfdcbcbf98faa7338
Sabbir Hossain <cse.170201039@gmail.com>
2023-06-10 21:15:14 +0600

    Complete Final Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+<h1>JAVASCRIPT TO-DO APP</h1>
+
+<p>Basic to-do app with create, complete and delete functionality.</p>
+
+<ol>
+  <li>Screenshot <br> <img src="./to-do-app.png" height="200px" width="300px"></li>
+</ol>
---
+++ script/script.js
@@ -0,0 +1,40 @@
+let input = document.getElementById("input");
+let list = document.getElementById("list");
+
+function addTask() {
+    if(input.value === "") {
+        alert("Input cant't be empty");
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = input.value;
+        list.append(li);
+
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    input.value = "";
+    saveData();
+}
+
+list.addEventListener("click", function(e) {
+    if(e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData() {
+    localStorage.setItem("ListData", list.innerHTML);
+}
+
+function showData() {
+    list.innerHTML = localStorage.getItem("ListData");
+}
+
+showData();
